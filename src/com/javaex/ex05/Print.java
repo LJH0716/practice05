24ea84d886ade6113601ac08b1547e25841c6d9d
LJH0716 <jhmk0805@naver.com>
2022-04-25 18:59:38 +0900

    2022-04-25 3

--- src/com/javaex/ex05/Print.java
+++ src/com/javaex/ex05/Print.java
@@ -2,4 +2,27 @@ package com.javaex.ex05;
 
 public class Print {
 
+	
+	 public void printer(int val){
+	        
+		 System.out.println(val);
+    }
+
+	 public void printer(boolean val) {
+		 if(val == true) {
+			 System.out.println(val);
+		 }
+		 else {
+			 System.out.println(false);
+		 }
+	 }
+	 
+	 public void printer(double val) {
+		 System.out.println(val);
+	 }
+	 
+	 public void printer(String val) {
+		 System.out.println(val);
+	 }
+	    
 }
--- src/com/javaex/ex06/CConverterApp.java
+++ src/com/javaex/ex06/CConverterApp.java
@@ -3,8 +3,17 @@ package com.javaex.ex06;
 public class CConverterApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		double dollar;
+        double won;
+        
+        CConverter.setRate(1118.70);
+        
+        //100만원을 달러로 출력하기
 
+        
+        //100달려를 원으로 출력하기
+	        
 	}
 
 }
